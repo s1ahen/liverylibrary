df265d8c3aa32f0d1ab072d988813320270acc19
s1ahen <potful_flak.0s@icloud.com>
2025-04-16 09:18:16 +0200

    Update script.js

--- script.js
+++ script.js
@@ -15,13 +15,13 @@ const cardsData = [
         title: 'C90B',
         description: 'The Pilot Club Livery for the C90B in X-plane',
         image: 'https://media.discordapp.net/attachments/1313249394618925099/1315304852297093171/Screenshot_2024-12-08_at_14.11.22.png?ex=6796dd59&is=67958bd9&hm=2d9435f07066361958e4a49829b47c3780e12c81c253a2337c13e7a5d8960d4a&=&format=webp&quality=lossless&width=1930&height=952',
-        buyLink: 'https://www.toyota.com/supra/build/'
+        buyLink: 'https://forums.x-plane.org/'
     },
     {
         title: 'C90B',
         description: 'The Pilot Club Livery for the C90B in X-plane',
         image: 'https://media.discordapp.net/attachments/1313249394618925099/1315304852297093171/Screenshot_2024-12-08_at_14.11.22.png?ex=6796dd59&is=67958bd9&hm=2d9435f07066361958e4a49829b47c3780e12c81c253a2337c13e7a5d8960d4a&=&format=webp&quality=lossless&width=1930&height=952',
-        buyLink: 'https://www.toyota.com/supra/build/'
+        buyLink: 'https://forums.x-plane.org/'
     },
 
     // Add more cards here
